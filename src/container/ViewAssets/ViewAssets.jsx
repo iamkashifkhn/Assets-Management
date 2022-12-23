e913c97d7a3716ada85bb82fce272bc32358d42a
iamkashifkhn <usmankashif544@gmail.com>
2022-12-23 14:55:02 +0500

    bug fixes

--- src/container/ViewAssets/ViewAssets.jsx
+++ src/container/ViewAssets/ViewAssets.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./viewAssets.css";
 import axios from 'axios';
+import { Spin} from "antd";
 import { useParams } from "react-router-dom";
 
 function ViewAssets() {
   const [data, setData ]= useState([])
+  const [loading, setLoading] = useState(true);
   const {id} = useParams()
   const apiURL = 'https://assets-management-6cdf7-default-rtdb.firebaseio.com/assets.json'
 
@@ -12,6 +14,7 @@ function ViewAssets() {
     try{
       const res = await axios.get(apiURL)
       console.log(res.data[id])
+      setLoading(!loading)
       setData(res.data[id])
     }
     catch(error) {
@@ -23,23 +26,30 @@ function ViewAssets() {
   },[])
   return (
     <div className="assets__view-container">
-      <div className="assets__view-content">
-      <h4>{data.employee}</h4>
-      <hr/>
-        <ul className="assets__view-content-list">
-           <li><span>Type:</span>{data.itemtype}</li>
-           <li><span>Model:</span>{data.model}</li>
-           {
-            data.itemtype==='Laptop'?
-            <>
-           <li><span>Ram:</span>{data.RAM}</li>
-           <li><span>Hard </span>{data.Hard}</li>
-           <li><span>IMEI </span>{data.IMEI}</li>
-            </>: null
-           }
+      {
+        loading?
+        <div className="assets__loader">
+          <Spin size="large" />
+        </div> :
+              <div className="assets__view-content">
+              <h4>{data.employee}</h4>
+              <hr/>
+                <ul className="assets__view-content-list">
+                   <li><span>Type:</span>{data.itemtype}</li>
+                   <li><span>Model:</span>{data.model}</li>
+                   {
+                    data.itemtype==='Laptop'?
+                    <>
+                   <li><span>Ram:</span>{data.RAM}</li>
+                   <li><span>Hard </span>{data.Hard}</li>
+                   <li><span>IMEI </span>{data.IMEI}</li>
+                    </>: null
+                   }
+        
+                </ul>
+                </div>
+      }
 
-        </ul>
-        </div>
     </div>
   );
 }
